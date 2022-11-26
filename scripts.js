59c28ea8a45b0f88af6ecc46d9860ba3069c76f0
Ariel <72635137+acsewer@users.noreply.github.com>
2022-11-26 06:14:48 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,19 +4,32 @@ const templateCard = document.getElementById("card-template").content;
 const fragment = document.createDocumentFragment();
 const btnCrear = document.getElementById("open-modal");
 const crearTask = document.getElementById("modal-overlay");
+const buscarField = document.getElementById("buscarId");
+const exitModal = document.getElementById("exitModal");
+
 /**Variables */
 let token = "";
 let user;
 let tasks;
 let taskSelected;
+let crearNuevaTarea = true;
 
 /**FRONTEND */
 
+exitModal.addEventListener("click", () => {
+  this.crearNuevaTarea = true;
+  crearTask.style.display = "none";
+  document.getElementById("crearBtnId").value = "Crear";
+});
+
+buscarField.addEventListener("change", (e) => {
+  getTasksID(e.target.value);
+});
+
 const removeTasks = () => {
   let cards = document.getElementsByClassName("card-wrapper");
-  console.log(cards.length);
+
   for (let i = 0; i < cards.length; i++) {
-    console.log(cards[i]);
     cards[i].remove();
     i = -1;
   }
@@ -24,31 +37,79 @@ const removeTasks = () => {
 function createTaskBtn() {
   const txtName = document.getElementById("fname").value;
   const txtDescrip = document.getElementById("fdescrp").value;
-  createTasks(txtName, txtDescrip);
+
+  if (this.crearNuevaTarea) {
+    createTasks(txtName, txtDescrip);
+    this.crearNuevaTarea = true;
+    document.getElementById("crearBtnId").value = "Crear";
+  } else {
+    updateTasks(this.taskSelected.id, txtName, txtDescrip);
+    this.crearNuevaTarea = true;
+    document.getElementById("crearBtnId").value = "Crear";
+  }
   crearTask.style.display = "none";
 }
 function closeModalNewTask() {
+  this.crearNuevaTarea = true;
   crearTask.style.display = "none";
+  document.getElementById("crearBtnId").value = "Crear";
 }
 
 btnCrear.addEventListener("click", () => {
+  document.getElementById("fname").value = "";
+  document.getElementById("fdescrp").value = "";
+
+  this.crearNuevaTarea = true;
   crearTask.style.display = "flex";
+  document.getElementById("crearBtnId").value = "Crear";
 });
 
 cardContiner.addEventListener("click", (e) => {
   let id = e.target.dataset.id;
-  if (id && e.target.innerText === "Eliminar") {
-    deleteTasks(id);
-  } else if (id && e.target.innerText === "Actualizar") {
-    this.taskSelected =  this.tasks.filter
+  if (id && e.target.id === "Eliminar") {
+    let opcion = confirm(`Seguro desea eliminar la tarea con id ${id}`);
+    if (opcion == true) {
+      deleteTasks(id);
+    }
+  } else if (id && e.target.id === "Actualizar") {
+    this.taskSelected = this.tasks.filter((e) => e.id == id)[0];
+    crearTask.style.display = "flex";
+
+    document.getElementById("fname").value = this.taskSelected.name;
+    document.getElementById("fdescrp").value = this.taskSelected.description;
+    document.getElementById("crearBtnId").value = "Actualizar";
+    this.crearNuevaTarea = false;
   }
 });
+
+cardContiner.addEventListener("change", (e) => {
+  let id = e.target.dataset.id;
+  let state = e.target.value;
+  changeStatusTasks(id, state);
+});
+
 const createCards = (data) => {
+  const arrayEstados = ["barra", " barra barra-st-1", " barra barra-st-2", "barra barra-st-3"];
+  const estados = ["", "Iniciado", "Pausa", "Terminado"]
+  let indexEstado = 0;
   data.forEach((task) => {
     templateCard.querySelector("h2").textContent = task.name;
     templateCard.querySelector("p").textContent = task.description;
     templateCard.querySelector(".btn-update").dataset.id = task.id;
     templateCard.querySelector(".btn-delete").dataset.id = task.id;
+    templateCard.querySelector(".estados").dataset.id = task.id;
+ 
+    if (task.status_id >= 0 && task.status_id <= 3) {
+      indexEstado = task.status_id;
+    }
+
+    if(!task.status_id){
+      indexEstado = 0;
+    }
+    
+    console.log(arrayEstados[indexEstado]);
+    templateCard.querySelector("#barra").className = arrayEstados[indexEstado];
+    templateCard.querySelector('#estado').innerText = estados[indexEstado];
     const clone = templateCard.cloneNode(true);
     fragment.appendChild(clone);
   });
@@ -101,7 +162,9 @@ const getAuthenticatedUser = async () => {
   res
     .then(async (result) => {
       await result.text().then((user) => {
-        console.log(user);
+        let usuario = JSON.parse(user)
+        console.log(usuario)
+        document.getElementById("txtLoginID").innerText = `Bienvenid@, ${usuario.name} (${usuario.email})`
         getTasks();
       });
     })
@@ -126,14 +189,52 @@ const getTasks = async () => {
     .then(async (result) => {
       await result.text().then((tasks) => {
         this.tasks = JSON.parse(tasks);
+
         removeTasks();
         createCards(this.tasks);
-        console.log(this.tasks);
       });
     })
     .catch((err) => {
       console.log(err);
     });
+};
+
+/**GET */
+const getTasksID = async (id = "") => {
+  if (id != "") {
+    const res = fetch(`https://tasks-crud.academlo.com/api/tasks/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        Accept: "application/json",
+      },
+    });
+    res
+      .then(async (result) => {
+        if (result.status == 200) {
+          await result.text().then((tasks) => {
+            if (tasks.length) {
+              this.tasks = [];
+              this.tasks.push(JSON.parse(tasks));
+
+              removeTasks();
+              createCards(this.tasks);
+            } else {
+              console.log(tasks);
+            }
+          });
+        } else {
+          getTasks();
+          alert(`No se encontro ningun valor asociado al id: ${id} `);
+          buscarField.value = "";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    getTasks();
+  }
 };
 
 /**POST */
@@ -176,7 +277,6 @@ const changeStatusTasks = async (id = 0, status = 0) => {
   res
     .then(async (result) => {
       await result.text().then((newTask) => {
-        console.log(newTask);
         getTasks();
       });
     })
@@ -197,6 +297,32 @@ const deleteTasks = async (id = 0) => {
   res
     .then(async (result) => {
       await result.text().then(() => {
+        getTasks();
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+/**PUT */
+const updateTasks = async (id = 0, name = "", descrp = "") => {
+  const res = fetch(`https://tasks-crud.academlo.com/api/tasks/${id}`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${this.token}`,
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      name: name,
+      description: descrp,
+    }),
+  });
+  res
+    .then(async (result) => {
+      await result.text().then((newTask) => {
+        console.log(newTask);
         getTasks();
       });
     })
